66efa901385537c533bebc9eb2d9430dd6f4de24
yeeunyuk <yy.eun6@gmail.com>
2022-11-17 04:52:09 +0900

    클래스와 객체 4장 12번

--- Chapter4/src/chap4/Concert.java
+++ Chapter4/src/chap4/Concert.java
@@ -2,11 +2,12 @@ package chap4;
 
 import java.util.Scanner;
 
+
 class Reserve {
 	Scanner sc = new Scanner(System.in);
-	String[] S = new String[40];
-	String[] A = new String[40];
-	String[] B = new String[40];
+	String[] S = new String[10];
+	String[] A = new String[10];
+	String[] B = new String[10];
 	
 	int chooseLine() {
 		System.out.print("좌석구분 S(1), A(2), B(3)>>");
@@ -19,58 +20,136 @@ class Reserve {
 		case 1:
 			System.out.print("S>>");
 			for(; i<S.length; i++) {
-				if (i%4 ==0) {
-					if (S[i] == null) {
-						System.out.print(" ");
-					}
-				} else System.out.print("_");
+				S[i] = "___ ";
+				System.out.print(S[i]);
 			}
+			chooseSeat(1);
 			break;
 		case 2:
 			System.out.print("A>>");
 			for(; i<A.length; i++) {
-				if (i%4 ==0) {
-					if (A[i] == null) {
-						System.out.print(" ");
-					}
-				} else System.out.print("_");
+				A[i] = "___ ";
+				System.out.print(A[i]);
 			}
+			chooseSeat(2);
 			break;
 		case 3:
 			System.out.print("B>>");
 			for(; i<B.length; i++) {
-				if (i%4 ==0) {
-					if (B[i] == null) {
-						System.out.print(" ");
-					}
-				} else System.out.print("_");
+				B[i] = "___ ";
+				System.out.print(B[i]);
 			}
+			chooseSeat(3);
 			break;
 		}
 	}
-	void chooseSeat() {
+	void chooseSeat(int line) {
 		System.out.println("");
 		System.out.print("이름>>");
 		String name = sc.next();
 		System.out.print("번호>>");
 		int seatNum = sc.nextInt();
-		Seat(seatNum);
+		seatNum--;
+
+		switch(line) {
+		case 1:
+			if (S[seatNum].equals("___ "))
+			S[seatNum] = name; 
+			break;
+		case 2:
+			if (A[seatNum].equals("___ ")) 
+			A[seatNum] = name;
+			break;
+		case 3:
+			if (B[seatNum].equals("___ ")) 
+			B[seatNum] = name;
+			break;
+		}
 	}
-	void Seat(int seatNum) {
+	void search () {	
+		System.out.print("S>>");
+		for(int i=0; i<S.length; i++) {
+			if (S[i] == null) {
+				S[i] = "___ ";
+			}
+			System.out.print(S[i]);
+		}
+		System.out.println();
+		System.out.print("A>>");
+		for(int i=0; i<A.length; i++) {
+			if (A[i] == null) {
+				A[i] = "___ ";
+			}
+			System.out.print(A[i]);
+		}
+		System.out.println();
+		System.out.print("B>>");
+		for(int i=0; i<B.length; i++) {
+			if (B[i] == null) {
+				B[i] = "___ ";
+			}
+			System.out.print(B[i]);
+		}
+	}
+	
+	void cancel(int line) {
+		switch (line) {
+		case 1:
+			System.out.print("S>>");
+			for(int i=0; i<S.length; i++) {
+				if (S[i] == null) {
+					S[i] = "___ ";
+				}
+				System.out.print(S[i]);
+			}
+			delete();
+			break;
+		case 2:
+			System.out.print("A>>");
+			for(int i=0; i<A.length; i++) {
+				if (A[i] == null) {
+					A[i] = "___ ";
+				}
+				System.out.print(A[i]);
+			}
+			delete();
+			break;
+		case 3:
+			System.out.print("B>>");
+			for(int i=0; i<B.length; i++) {
+				if (B[i] == null) {
+					B[i] = "___ ";
+				}
+				System.out.print(B[i]);
+			}
+			delete();
+			break;
+		}
 		
 	}
-}
-class Search {
-	
-}
-class Cancel {
-	
+	void delete () {
+		System.out.println();
+		System.out.print("이름>>");
+		String name = sc.next();
+		
+		for(int i=0; i<S.length; i++) {
+			if(name.equals(S[i])) {
+				S[i] = "___ ";
+			}else if(name.equals(A[i])) {
+				A[i] = "___ ";
+			}else if(name.equals(B[i])) {
+				B[i] = "___ ";
+			}
+		}
+	}
+
 }
 
 public class Concert {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Reserve reserve = new Reserve();
 		int num = 0;
 		System.out.println("명품콘서트홀 예약 시스템입니다.");
 		
@@ -80,13 +159,15 @@ public class Concert {
 			
 			switch(num) {
 			case 1:
-				Reserve reserve = new Reserve();
 				reserve.Line(reserve.chooseLine());
-				reserve.chooseSeat();
 				break;
 			case 2:
+				reserve.search();
+				System.out.println();
+				System.out.println("<<<조회를 완료하였습니다.>>>");
 				break;
 			case 3:
+				reserve.cancel(reserve.chooseLine());
 				break;
 			case 4:
 				break;
